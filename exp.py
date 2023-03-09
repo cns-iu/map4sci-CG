7d30c0fab7c086c5485612cb349c712ce6a0f18d
akhileshgowda <akhileshgowda7@gmail.com>
2023-03-09 17:39:45 -0500

    cleaned up code and converted input format to csv

--- exp.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# Open the JSON file
-with open('./examples/inputs/initbtreeSciMap.json', 'r') as f:
-    # Load the JSON data
-    data = json.load(f)
-
-# Access the data
-print(data)
